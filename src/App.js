313b647c99bc124fa2f14909f0ff65e8bd8d2cb2
Aashay <arkhamknight.aashay@gmail.com>
2022-03-12 19:49:53 +0530

    added dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
-export default function App(){
+export default function App() {
     const items = [{
         title: 'what is react?',
         content: 'front end js framework'
@@ -15,8 +16,21 @@ export default function App(){
         title: 'how to react?',
         content: 'by creating components'
     }]
+
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Green', value: 'green' },
+        { label: 'Blue', value: 'blue' },
+    ]
+
+    const [selected, setSelected] = useState(options[0])
+
     return <div>
         <Accordion items={items} />
         <Search />
+        <Dropdown
+            onSelectedChange={setSelected}
+            selected={selected}
+            options={options} />
     </div>
 }
