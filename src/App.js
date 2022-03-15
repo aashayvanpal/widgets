f55d6953e37fd5758ae0a826a499659c6497a6b7
Aashay <arkhamknight.aashay@gmail.com>
2022-03-15 12:04:07 +0530

    added custom routing component

--- src/App.js
+++ src/App.js
@@ -3,32 +3,48 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
+
+const items = [{
+    title: 'what is react?',
+    content: 'front end js framework'
+},
+{
+    title: 'why react?',
+    content: 'faviorate library'
+},
+{
+    title: 'how to react?',
+    content: 'by creating components'
+}]
+
+const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+]
+
 
 export default function App() {
-    const items = [{
-        title: 'what is react?',
-        content: 'front end js framework'
-    },
-    {
-        title: 'why react?',
-        content: 'faviorate library'
-    },
-    {
-        title: 'how to react?',
-        content: 'by creating components'
-    }]
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' },
-    ]
-
+    const [selected, setSelected] = useState(options[0])
     return <div>
-        {/* <Accordion items={items} />
-        <Search /> */}
+        <Route path="/">
+            <Accordion items={items} />
+        </Route>
 
-        <Translate />
-        
+        <Route path="/list">
+            <Search />
+        </Route>
+
+        <Route path="/dropdown">
+            <Dropdown label="select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected} />
+        </Route>
+
+        <Route path="/translate">
+            <Translate />
+        </Route>
     </div>
 }
